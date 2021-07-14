b3a3ef4205d2380cf08528eb1dc6a17aa90aa02f
Muhimen123 <almuhimen4@gmail.com>
2021-07-14 11:20:11 +0600

    :sparkles: Command Picker
    If no default argument is provided, main script will show a command
    picker to choose a command from the list.

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import sys
 import os
+from PyInquirer import prompt
 from utils import commandController
 
 
 arguments = sys.argv
 if len(arguments) < 2:
-    print("Not enough arguments provided")
-    # TODO: Replace this with help screen
-else:
-    commandController.command_controller(arguments)
+    questions = [
+        {
+            "type": "list",
+            "name": "command",
+            "message": "Choose a command to apply",
+            "choices": [
+                "browse",
+                "download",
+                "help",
+                "save",
+                "search",
+                "view"
+            ]
+        }
+    ]
+
+    answer = prompt(questions)["command"]
+    arguments.append(answer)
+
+commandController.command_controller(arguments)
