fd295c33ca49c0264aafe1cf2bd60f65d759a8d2
Muhimen123 <almuhimen4@gmail.com>
2021-07-14 08:18:43 +0600

    :recycle: Refactor Code
    Refactored TID repo image selection prompt to use in other scripts.

--- utils/imageViewer.py
+++ utils/imageViewer.py
@@ -29,27 +29,9 @@ def image_viewer():
     answer = prompt(questions)["view_type"]
 
     if answer == "TID Repo":
-        entry_number = int(input("Please enter image number: ")) - 1
-
-        try:
-            response = requests.get(image_data_url)
-
-            if response.status_code == 200:
-                image_data = response.json()
-                if entry_number >= len(image_data):
-                    entry_number = 1
-
-                file_name = image_data[entry_number]["image_name"]
-                file_type = image_data[entry_number]["file_type"]
-                base_url = "https://raw.githubusercontent.com/Muhimen123/TID/main/images/"
-                link = base_url + file_name + "." + file_type
-
-            else:
-                print(Fore.RED + f"Oops, something went wrong {response.status_code}" + Style.RESEST_ALL)
-
-        except Exception as error:
-            print(error)
-            print(Fore.RED + "Perhaps you are not connected to the internet. Mind checking it again?")
+        link = tid_repo_prompt(image_data_url)
+        if link is None:
+            return -1
 
     else:
         link = input("Please enter download link: ")
@@ -59,4 +41,40 @@ def image_viewer():
 
     img = Image.open(path + "tmp_wallpaper_preview_image.png")
     img.show()
+
+
+def tid_repo_prompt(image_data_url: str):
+
+    """
+    Prompt for TID repo image selection
+    :param image_data_url: base url of the TID repo imaegs
+    :return: download link of the image or None
+    """
+
+    entry_number = int(input("Please enter image number: ")) - 1
+    link = None
+
+    try:
+        response = requests.get(image_data_url)
+
+        if response.status_code == 200:
+            image_data = response.json()
+            if entry_number >= len(image_data):
+                entry_number = 1
+
+            file_name = image_data[entry_number]["image_name"]
+            file_type = image_data[entry_number]["file_type"]
+            base_url = "https://raw.githubusercontent.com/Muhimen123/TID/main/images/"
+            link = base_url + file_name + "." + file_type
+
+            return link
+
+        else:
+            print(Fore.RED + f"Oops, something went wrong {response.status_code}" + Style.RESEST_ALL)
+
+    except Exception as error:
+        print(error)
+        print(Fore.RED + "Perhaps you are not connected to the internet. Mind checking it again?")
+    
+    return link
 
